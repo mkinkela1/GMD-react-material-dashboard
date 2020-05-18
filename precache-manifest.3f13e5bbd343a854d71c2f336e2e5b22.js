cc58e7395e4a08388cf319d5dfbb94af3e55c777
Matteo Kinkela <matteo.kinkela@flipkod.com>
2020-05-18 08:58:14 +0200

    Updates

--- precache-manifest.3f13e5bbd343a854d71c2f336e2e5b22.js
+++ precache-manifest.3f13e5bbd343a854d71c2f336e2e5b22.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f75380b12a180dff26288b4db6d2724d",
+    "revision": "30515805aa2daf5d678760089847b3d4",
     "url": "/GMD-react-material-dashboard/index.html"
   },
   {
-    "revision": "1785028a82e487b4fd87",
+    "revision": "670cade422cb1dc1f5c8",
     "url": "/GMD-react-material-dashboard/static/css/2.18fb2ca0.chunk.css"
   },
   {
-    "revision": "3264b89c2ed073a58861",
+    "revision": "955c36dcbde651319423",
     "url": "/GMD-react-material-dashboard/static/css/main.fec45a73.chunk.css"
   },
   {
-    "revision": "1785028a82e487b4fd87",
-    "url": "/GMD-react-material-dashboard/static/js/2.a1f1ddd9.chunk.js"
+    "revision": "670cade422cb1dc1f5c8",
+    "url": "/GMD-react-material-dashboard/static/js/2.60c78d10.chunk.js"
   },
   {
     "revision": "82fd2133711d1e027fde858069bc8536",
-    "url": "/GMD-react-material-dashboard/static/js/2.a1f1ddd9.chunk.js.LICENSE.txt"
+    "url": "/GMD-react-material-dashboard/static/js/2.60c78d10.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3264b89c2ed073a58861",
-    "url": "/GMD-react-material-dashboard/static/js/main.6e80613a.chunk.js"
+    "revision": "955c36dcbde651319423",
+    "url": "/GMD-react-material-dashboard/static/js/main.215de8c4.chunk.js"
   },
   {
     "revision": "52418541da624b8d4e3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GMD-react-material-dashboard/precache-manifest.2f1e66f02c87cb5b1a09cc87a2f32237.js"
+  "/GMD-react-material-dashboard/precache-manifest.3f13e5bbd343a854d71c2f336e2e5b22.js"
 );
 
 self.addEventListener('message', (event) => {
